f7ef003ad42ad53c7ed19ca9bd728438cd168c0a
Bishop Lopez <dynamiteboy@protonmail.com>
2025-09-14 12:21:18 -0500

    create Advocate type

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface Advocate {
+  id: number;
+  firstName: string;
+  lastName: string;
+  city: string;
+  degree: string;
+  specialties: string[];
+  yearsOfExperience: number;
+  phoneNumber: number;
+  createdAt?: Date;
+}
+
+export type AdvocateInput = Omit<Advocate, 'id' | 'createdAt'>;
