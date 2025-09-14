97fea563a69d330e7686c9701c7c29592b319bdc
Bishop Lopez <dynamiteboy@protonmail.com>
2025-09-14 18:45:10 -0500

    add auth to route

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -2,11 +2,11 @@ import db from "../../../db";
 import { advocates } from "../../../db/schema";
 
 export async function GET(request: Request) {
-  // // Basic auth check
-  // const authHeader = request.headers.get('authorization');
-  // if (authHeader !== `Bearer ${process.env.ADMIN_API_KEY}`) {
-  //   return Response.json({ error: 'Unauthorized' }, { status: 401 });
-  // }
+  // Basic auth check
+  const authHeader = request.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.ADMIN_API_KEY}`) {
+    return Response.json({ error: 'Unauthorized' }, { status: 401 });
+  }
 
   try {
     const data = await db.select().from(advocates);
