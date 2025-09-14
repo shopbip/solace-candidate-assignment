84dbf20e8f9f3ce4ab8374063c78dc0f274c6871
Bishop Lopez <dynamiteboy@protonmail.com>
2025-09-14 12:35:15 -0500

    make seeding idempotent

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -3,6 +3,10 @@ import { advocates } from "../../../db/schema";
 import { advocateData } from "../../../db/seed/advocates";
 
 export async function POST() {
+  // Remove all existing advocates to make seeding idempotent
+  await db.delete(advocates);
+
+  // Insert new advocate data
   const records = await db.insert(advocates).values(advocateData).returning();
 
   return Response.json({ advocates: records });
