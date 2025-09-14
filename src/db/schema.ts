b322a448cb9d52ebaff5a15f3a4ab215b908efb7
Bishop Lopez <dynamiteboy@protonmail.com>
2025-09-14 17:59:19 -0500

    change column name form "payload" to "specialties"

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -14,7 +14,7 @@ const advocates = pgTable("advocates", {
   lastName: text("last_name").notNull(),
   city: text("city").notNull(),
   degree: text("degree").notNull(),
-  specialties: jsonb("payload").default([]).notNull(),
+  specialties: jsonb("specialties").default([]).notNull(),
   yearsOfExperience: integer("years_of_experience").notNull(),
   phoneNumber: text("phone_number").notNull(),
   createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
